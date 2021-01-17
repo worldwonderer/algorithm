7cb7be444906bc6b9f546082e2b6a298ee9c5dbf
worldwonderer <xtchen.pitt@gmail.com>
2021-01-17 22:22:37 +0800

    add solution

---
+++ 剑指Offer/剑指 Offer 21. 调整数组顺序使奇数位于偶数前面.py
@@ -0,0 +1,9 @@
+class Solution:
+    def exchange(self, nums: List[int]) -> List[int]:
+        fast, slow = 0, 0
+        while fast < len(nums):
+            if nums[fast] % 2 == 1:
+                nums[slow], nums[fast] = nums[fast], nums[slow]
+                slow += 1
+            fast += 1
+        return nums
