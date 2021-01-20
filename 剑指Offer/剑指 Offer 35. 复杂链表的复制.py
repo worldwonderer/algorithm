9b9442e933886f96050ca7dd7cca7bc49351c9eb
worldwonderer <xtchen.pitt@gmail.com>
2021-01-20 22:03:26 +0800

    add solution

---
+++ 剑指Offer/剑指 Offer 35. 复杂链表的复制.py
@@ -0,0 +1,25 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
+class Solution:
+    def copyRandomList(self, head: 'Node') -> 'Node':
+        visited = {}
+
+        def dfs(node):
+            if node is None:
+                return None
+            if node in visited:
+                return visited[node]
+
+            copy = Node(node.val, None, None)
+            visited[node] = copy
+            copy.next = dfs(node.next)
+            copy.random = dfs(node.random)
+            return copy
+
+        return dfs(head)
